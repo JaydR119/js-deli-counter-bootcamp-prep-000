172a7640d1700d92b3fe5346ed5cf64dddda7f2b
Jayd Robinson <jaydr119@gmail.com>
2019-08-28 11:55:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,4 +20,10 @@ function currentLine(katzDeli) {
   if (katzDeli.length === 0) {
     return "The line is currently empty."
   }
+  var customerAndNumber = [];
+  for (var i=0; i<katzDeli.length; i++) {
+    customerAndNumber.push(i+1 + ". " + katzDeli[i])
+  }
+  return "The line is currently: " + customerAndNumber.join(", ");
 }
+
